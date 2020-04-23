4f77c76cc506d56b0d2506c6b7d1f404e5567645
liuxiangbin <liuxiangbin@followme.cn>
2020-04-23 15:46:09 +0800

    1，在线交易统计，user_accounts/t_users表DIR同步到mongodb有严重反压，会完全卡死不消费。fmstreaming如下优化：在mapbfaf里取消经纪商时区转换，因为没有tsdb转换无用（性能改进最大）。libcomplexstr如下优化：a，before/update都存在改单情况判断是否有关注修改；b，更新mongodb时如果不要求比较老值就不查询。c，redis partition，offset不再保存。libcdcdwstr如下优化：kafka key无用不反序列化。

--- src/main/java/at/bronzels/libcdcdwstr/flink/source/KafkaDeserializationJsonNodeSchema.java
+++ src/main/java/at/bronzels/libcdcdwstr/flink/source/KafkaDeserializationJsonNodeSchema.java
@@ -1,26 +1,57 @@
 package at.bronzels.libcdcdwstr.flink.source;
 
 import at.bronzels.libcdcdwstr.bean.SourceRecordKafkaJsonNode;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectReader;
+import org.apache.flink.streaming.util.serialization.JSONKeyValueDeserializationSchema;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 public class KafkaDeserializationJsonNodeSchema extends AbstractKafkaDeserializationSchema<SourceRecordKafkaJsonNode> {
     private final Logger LOG = LoggerFactory.getLogger(getClass());
 
     private ObjectMapper mapper = new ObjectMapper();
+    //private ObjectReader reader = new ObjectMapper().reader();
+    //private SimpleStringSchema strSchema = new SimpleStringSchema();
+    //private JSONKeyValueDeserializationSchema nodeSchema = new JSONKeyValueDeserializationSchema(true);
 
     @Override
     public SourceRecordKafkaJsonNode deserialize(byte[] keyBytes, byte[] valueBytes, String topic, int partition, long offset) throws IOException {
         long msgts = System.currentTimeMillis();
         String guid = String.valueOf(partition) + at.bronzels.libcdcdw.Constants.commonSep + offset;
         LOG.debug("guid:{}, msgts:{}, topic:{}, partition:{}, offset:{}", guid, msgts, topic, partition, offset);
-        String key = keyBytes != null ? new String(keyBytes, "UTF-8") : null;
+        String key = "";//keyBytes != null ? new String(keyBytes, "UTF-8") : null;
+        long msgts_startStr = System.currentTimeMillis();
+        //String strValue = new String(valueBytes, "UTF-8");
+        //String strValue = new String(valueBytes, StandardCharsets.US_ASCII);
+        //String strValue = valueBytes != null ? new String(valueBytes) : null;
+        //String strValue = valueBytes != null ? new String(valueBytes, StandardCharsets.UTF_16) : null;
+        //String strValue = valueBytes != null ? strSchema.deserialize(valueBytes) : null;
+        long msgts_takenStr = System.currentTimeMillis() - msgts_startStr;
+        LOG.debug("guid:{}, msgts_takenStr:{}", guid, msgts_takenStr);
+        long msgts_start = System.currentTimeMillis();
         JsonNode value = valueBytes != null ? mapper.readTree(valueBytes) : null;
+        //JsonNode value = valueBytes != null ? mapper.readTree(strValue) : null;
+        //JsonNode value = (offset == 192 || offset == 215 | offset == 729) ? null : (valueBytes != null ? reader.readTree(new ByteArrayInputStream(valueBytes)) : null);
+        /*
+        JsonNode value = null;
+        try{
+            value = valueBytes != null ? nodeSchema.deserialize(new ConsumerRecord<byte[], byte[]>(topic, partition, offset, keyBytes, valueBytes)).get("value") : null;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+         */
+        long msgts_taken = System.currentTimeMillis() - msgts_start;
+        LOG.debug("guid:{}, msgts_taken:{}", guid, msgts_taken);
         LOG.debug("guid:{}, ctmilli:{}", guid, System.currentTimeMillis());
 
         SourceRecordKafkaJsonNode record = new SourceRecordKafkaJsonNode(key, value, topic, partition, offset, msgts, guid);
